4900e5bb11872ed1b22ccfd7daa16d6242f9c1a6
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-08-31 18:10:47 +0530

    Update mongodb.js

--- backend/db/mongodb.js
+++ backend/db/mongodb.js
@@ -2,7 +2,7 @@ import  mongoose from "mongoose";
 
 const connectMongoDB = async() =>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect("mongodb+srv://mukulved07:Tav5SRuyZppKtzoL@cluster0.7ebyt.mongodb.net/twitter-db");
         console.log("MongoDB connected");
 
     }catch(error){
