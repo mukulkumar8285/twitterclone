6395b8860aecb56223a8ebeb34f545665dd85df4
Mukul Kumar <149408801+mukulkumar8285@users.noreply.github.com>
2024-08-31 17:40:53 +0530

    Update ProfilePage.jsx

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -39,7 +39,7 @@ const ProfilePage = () => {
 		queryKey: ["userProfile"],
 		queryFn: async () => {
 			try {
-				const res = await fetch(`http://localhost:8080/api/users/profile/${username}`,{
+				const res = await fetch(`https://twitterclone-backend-iota.vercel.app/api/users/profile/${username}`,{
 					credentials:"include"
 				});
 				const data = await res.json();
